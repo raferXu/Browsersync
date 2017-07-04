16951087b9cee77171cb2136451730b767ade189
raferXu <raferxu@163.com>
2017-07-04 16:23:54 +0800

    新分支只留需要修改pybossa逻辑的代码

--- webpack sample project/app/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by raferxu on 17/5/28.
- */
-var greeter = require('./Greeter2.js');
-document.getElementById('root').appendChild(greeter());
--- zhongbao/recordMp3/upload.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" CodeBehind="upload.ashx.cs" Class="h5.audio.upload" %>
