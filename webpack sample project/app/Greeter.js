b6aa7e9957dfd97edd16427d7e2ab261bce63de1
raferXu <raferxu@163.com>
2017-07-13 18:15:42 +0800

    xxxxxx

--- webpack sample project/app/Greeter.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by raferxu on 17/5/28.
- */
-module.exports = function () {
-  var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
-  return greet;
-};
--- webpack sample project/app/Greeter2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by raferxu on 17/5/28.
- */
-var config = require('./config.json');
-module.exports = function () {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
